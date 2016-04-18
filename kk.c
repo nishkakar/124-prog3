c7ec00ed6c29c91d99cf9beabf3f1e141e082b99
Nishant Kakar <nishant@neurotrack.com>
2016-04-18 16:49:09 -0400

    changed to long long

--- kk.c
+++ kk.c
@@ -11,18 +11,18 @@
 int main(int argc, char* argv[]) {
     char* inputfile = argv[1];
 
-    int* input = malloc(100 * sizeof(int));
+    long long* input = malloc(100 * sizeof(int));
 
     FILE* fp;
     fp = fopen(inputfile, "r");
 
     for (int i = 0; i < 100; i++) {
         printf("allocates # %d\n", i);
-        fscanf(fp, "%d", &input[i]);
+        fscanf(fp, "%lld", &input[i]);
     }
 
     // test to see if loading input worked
     for (int i = 0; i < 100; i++) {
-        printf("%c\n", input[i]);
+        printf("%lld\n", input[i]);
     }
 }
