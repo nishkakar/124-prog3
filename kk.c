1ec4510283d9e0a7efc46f1cf8f9968ab7cd5651
Nishant Kakar <nishant@neurotrack.com>
2016-04-18 16:46:32 -0400

    fixedd stupid big

--- kk.c
+++ kk.c
@@ -9,23 +9,20 @@
 #include <math.h>
 
 int main(int argc, char* argv[]) {
-    char* inputfile = argv[2];
+    char* inputfile = argv[1];
 
     int* input = malloc(100 * sizeof(int));
 
     FILE* fp;
     fp = fopen(inputfile, "r");
 
-    char buf[256];
     for (int i = 0; i < 100; i++) {
         printf("allocates # %d\n", i);
-        fgets(buf, sizeof(buf), fp);
-        printf("reads\n");
-        input[i] = atoi(buf);
+        fscanf(fp, "%d", &input[i]);
     }
 
     // test to see if loading input worked
-    // for (int i = 0; i < 100; i++) {
-    //     printf("%c\n", input[i]);
-    // }
+    for (int i = 0; i < 100; i++) {
+        printf("%c\n", input[i]);
+    }
 }
